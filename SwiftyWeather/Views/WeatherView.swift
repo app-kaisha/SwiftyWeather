c6e1952ee95350b9b4c57614157a701c068ab94f
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-11 18:29:26 +0000

    Basic Main View UI created

--- SwiftyWeather/Views/WeatherView.swift
+++ SwiftyWeather/Views/WeatherView.swift
@@ -5,18 +5,45 @@
 //  Created by app-kaihatsusha on 11/01/2026.
 //  Copyright © 2026 app-kaihatsusha. All rights reserved.
 //
+//
+//
 
 import SwiftUI
 
 struct WeatherView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                Color.cyan.opacity(0.75)
+                    .ignoresSafeArea()
+                VStack {
+                    Image(systemName: "cloud.sun.rain.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .symbolRenderingMode(.multicolor)
+                        .padding(.horizontal)
+                    Text("Wild Weather")
+                        .font(.largeTitle)
+                    Text("42°C")
+                        .font(.system(size: 150, weight: .thin))
+                    Text("Wind 10mph - Feels Like 36°C")
+                        .font(.title2)
+                        .padding(.bottom)
+                }
+                .foregroundStyle(.white)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "gear")
+                                
+                        }
+                        .tint(.white)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
