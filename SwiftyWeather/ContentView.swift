479f4ef47f4bf89629a06098e7ec9771bac62b15
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-11 17:55:24 +0000

    Initial Commit

---
+++ SwiftyWeather/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftyWeather
+//
+//  Created by app-kaihatsusha on 11/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftyWeather/SwiftyWeatherApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftyWeatherApp.swift
+//  SwiftyWeather
+//
+//  Created by app-kaihatsusha on 11/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
